d7d74962b5705b988393c54dccbfba89aa4fe79d
XX <xx@XXs-MBP13.local>
2019-06-10 16:12:59 -0400

    changed to my default font

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ button.addEventListener("click", async () => {
     zoom: 2
   });
 
-  map.addControl(new mapboxgl.NavigationControl());
+  // map.addControl(new mapboxgl.NavigationControl());
 
   map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png', function (error, image) {
     if (error) throw error;
@@ -71,68 +71,6 @@ button.addEventListener("click", async () => {
 });
 
 
-//end of ant coordinates
-
-
-//map
-
-
-
-/* given a query in the form "lng, lat" or "lat, lng" returns the matching
-* geographic coordinate(s) as search results in carmen geojson format,
-* https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
-*/
-// var coordinatesGeocoder = function (query) {
-  // match anything which looks like a decimal degrees coordinate pair
-  // var matches = query.match(/^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i);
-  // if (!matches) {
-  //   return null;
-  // }
-
-  // function coordinateFeature(lng, lat) {
-  //   return {
-  //     center: [lng, lat],
-  //     geometry: {
-  //       type: "Point",
-  //       coordinates: [lng, lat]
-  //     },
-  //     place_name: 'Lat: ' + lat + ' Lng: ' + lng, // eslint-disable-line camelcase
-  //     place_type: ['coordinate'], // eslint-disable-line camelcase
-  //     properties: {},
-  //     type: 'Feature'
-  //   };
-  // }
-
-  // var coord1 = Number(matches[1]);
-  // var coord2 = Number(matches[2]);
-  // var geocodes = [];
-
-  // if (coord1 < -90 || coord1 > 90) {
-  //   // must be lng, lat
-  //   geocodes.push(coordinateFeature(coord1, coord2));
-  // }
-
-  // if (coord2 < -90 || coord2 > 90) {
-  //   // must be lat, lng
-  //   geocodes.push(coordinateFeature(coord2, coord1));
-  // }
-
-  // if (geocodes.length === 0) {
-  //   // else could be either lng, lat or lat, lng
-  //   geocodes.push(coordinateFeature(coord1, coord2));
-  //   geocodes.push(coordinateFeature(coord2, coord1));
-  // }
-
-//   return geocodes;
-// };
-
-// map.addControl(new MapboxGeocoder({
-//   accessToken: mapboxgl.accessToken,
-//   localGeocoder: coordinatesGeocoder,
-//   zoom: 4,
-//   placeholder: "Try: -40, 170",
-//   mapboxgl: mapboxgl
-// }));
 
 
 
