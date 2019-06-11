896a9d82cf64077f8c704ed71e46111318da7725
XX <xx@XXs-MBP13.local>
2019-06-11 17:48:57 -0400

    fixed text format

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+///API credential
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmFsbHdhbGwiLCJhIjoiY2p3cWdsc25sMTlpZDN4bWc3d2ttNjFyYSJ9.MbCUR8jEU5qasNsLFkKBqA';
-
 const path = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+///UI element
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const noise = document.querySelector("#noise");
 const description = document.querySelector("#exp");
 
 
-//beginning off ant coordinates
+//button event listener begins
 button.addEventListener("click", async () => {
-  noise.style.display = "none";
-  const searchable = input.value.replace(" ", "%20");
+  description.innerHTML = "";     //clear previous search result
+  noise.style.display = "none";     //remove noise effect once search initiates
+  const searchable = input.value.replace(" ", "%20");     //format entered location
   const response = await axios.get(`${path}${searchable}.json?access_token=${mapboxgl.accessToken}`)
-  const coordinates = response.data.features[0].geometry.coordinates;
-  let lng = coordinates[0];
-  let lat = coordinates[1];
+  const coordinates = response.data.features[0].geometry.coordinates;     //return the array of coordiantes
+  let lng = coordinates[0];     //latitude
+  let lat = coordinates[1];     //longitude
+  //below: function to covert coordinate to antipode's coordinate
   const lngConvert = (lng) => {
     if (lng > 0) {
       return lng - 180;
@@ -23,29 +26,34 @@ button.addEventListener("click", async () => {
     }
   }
   console.log(lng, lat);
-  antlat = -lat;
-  antlng = lngConvert(lng);
-
+  antlat = -lat;            //antipode's latitude
+  antlng = lngConvert(lng); //antipode's longitude 
+  //below: second API call from antipodes' coordinates;
   const antresponse = await axios.get(`${path}${antlng},${antlat}.json?types=country&access_token=${mapboxgl.accessToken}`);
-  const countryname = antresponse.data.features[0].place_name;
+  console.log(antresponse);
+  const countryname = antresponse.data.features;   ///return the array of into, could be none
+  let countryitem;
+  /// if location is not in a specific country
+  if (countryname.length) {     //empty array returns 0;
+    countryitem = countryname[0].place_name;
+  } else {
+    countryitem = "somewhere in the water";
+  }
 
-
+  ///formating antipode's country name and coordinates;
   const antlat3 = antlat.toFixed(3);
   const antlng3 = antlng.toFixed(3);
   const placename = input.value.toUpperCase();
-
-
-  const country = (acountryname === undefined ? "somewhere in the water." : countryname);
-  const countryUpper = country.toUpperCase();
-
+  const countryUpper = countryitem.toUpperCase();
+  ///display antipode's info in text
   const desc = document.createElement("div");
   desc.innerHTML = `<br>
-  The antipode of ${placename} is (${antlat3}, ${antlng3}).
+  The antipode of ${placename} is (${antlat3}, ${antlng3}). <br>
   That's ${countryUpper}.
   `
   description.append(desc);
 
-
+  ///mapbox stuff;
   var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
@@ -54,7 +62,7 @@ button.addEventListener("click", async () => {
   });
 
 
-
+  ///mapbox: add icon to antipodes' coordinate;
   map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png', function (error, image) {
     if (error) throw error;
     map.addImage('cat', image);
