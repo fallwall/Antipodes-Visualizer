7f1aa332861a5c70b576da5b7ecddf709cf826b8
XX <xx@XXs-MBP13.local>
2019-06-11 16:51:40 -0400

    visual enhancement and second axios call

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZmFsbHdhbGwiLCJhIjoiY2p3cWdsc25sMTlpZDN4bWc3d
 const path = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
 const input = document.querySelector("input");
 const button = document.querySelector("button");
-// const originalmap = document.querySelector("#map1");
+const noise = document.querySelector("#noise");
+const description = document.querySelector("#exp");
 
-// let antlat;
-// let antlng;
 
 //beginning off ant coordinates
 button.addEventListener("click", async () => {
+  noise.style.display = "none";
   const searchable = input.value.replace(" ", "%20");
   const response = await axios.get(`${path}${searchable}.json?access_token=${mapboxgl.accessToken}`)
-
   const coordinates = response.data.features[0].geometry.coordinates;
   let lng = coordinates[0];
   let lat = coordinates[1];
@@ -27,6 +26,24 @@ button.addEventListener("click", async () => {
   antlat = -lat;
   antlng = lngConvert(lng);
 
+  const antresponse = await axios.get(`${path}${antlng},${antlat}.json?types=country&access_token=${mapboxgl.accessToken}`);
+  const countryname = antresponse.data.features[0].place_name;
+
+
+  const antlat3 = antlat.toFixed(3);
+  const antlng3 = antlng.toFixed(3);
+  const placename = input.value.toUpperCase();
+
+
+  const country = (acountryname === undefined ? "somewhere in the water." : countryname);
+  const countryUpper = country.toUpperCase();
+
+  const desc = document.createElement("div");
+  desc.innerHTML = `<br>
+  The antipode of ${placename} is (${antlat3}, ${antlng3}).
+  That's ${countryUpper}.
+  `
+  description.append(desc);
 
 
   var map = new mapboxgl.Map({
