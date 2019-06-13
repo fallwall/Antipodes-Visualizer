20cac31208619904bcf9180f14ab4bc2a9ea41e4
XX <xx@XXs-MBP13.local>
2019-06-13 10:36:40 -0400

    changed back to online cat icon. no changes

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ button.addEventListener("click", async () => {
 
 
   ///mapbox: add icon to antipodes' coordinate;
-  map.loadImage('cat.png', function (error, image) {
+  map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png', function (error, image) {
     if (error) throw error;
     map.addImage('cat', image);
     map.addLayer({
